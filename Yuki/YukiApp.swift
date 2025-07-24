14e441312e0caf2f93806c2d5164aeb117d7f6f6
Dambadarjaa <57674891+Antaqor@users.noreply.github.com>
2025-07-24 13:46:43 +0800

    Initial Commit

---
+++ Yuki/YukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YukiApp.swift
+//  Yuki
+//
+//  Created by Tusudei on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct YukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YukiTests/YukiTests.swift
@@ -0,0 +1,17 @@
+//
+//  YukiTests.swift
+//  YukiTests
+//
+//  Created by Tusudei on 7/24/25.
+//
+
+import Testing
+@testable import Yuki
+
+struct YukiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
