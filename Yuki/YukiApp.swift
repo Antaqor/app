a9be6fd72c888fd326e1b985ae2eb4898fcd1b32
Dambadarjaa <57674891+Antaqor@users.noreply.github.com>
2025-07-25 13:47:12 +0800

    t

--- Yuki/YukiApp.swift
+++ Yuki/YukiApp.swift
@@ -1,17 +1,24 @@
-//
-//  YukiApp.swift
-//  Yuki
-//
-//  Created by Tusudei on 7/24/25.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
-struct YukiApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
